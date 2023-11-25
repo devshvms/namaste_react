5ed96f46f699351488bb5e9f06686ae8f29027a4
shvms <dev.shvms@gmail.com>
2023-11-26 00:30:43 +0530

    EP-2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading1 = React.createElement("h1", {id: "heading1"}, "Hello World from React!!");
 const heading2 = React.createElement("h2", {id: "heading2"}, "Hello World from React!!");
 
